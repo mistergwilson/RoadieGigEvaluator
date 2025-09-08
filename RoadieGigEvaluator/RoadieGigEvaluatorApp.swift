adf29c902f7f986a22d53f3e9b9a09a99b73b458
Gregory Wilson <mistergwilson@gmail.com>
2025-09-08 13:57:19 -0700

    Initial Commit

---
+++ RoadieGigEvaluator/RoadieGigEvaluatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadieGigEvaluatorApp.swift
+//  RoadieGigEvaluator
+//
+//  Created by Admin on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RoadieGigEvaluatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
